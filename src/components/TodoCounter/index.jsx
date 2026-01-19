edab708e69fc3d7ae67a6f02b23d8b036043c277
YorberR <yorberrojas15@gmail.com>
2026-01-18 22:08:34 -0400

    feat: add TodoCounter component to display the task manager title and current date.

--- src/components/TodoCounter/index.jsx
+++ src/components/TodoCounter/index.jsx
@@ -1,21 +1,21 @@
 import './TodoCounter.css';
 
 function TodoCounter() {
-    
+
     const getCurrentDate = () => {
         const today = new Date();
-        const options = { 
-            weekday: 'long', 
-            year: 'numeric', 
-            month: 'long', 
-            day: 'numeric' 
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
         };
         return today.toLocaleDateString('es-ES', options);
     };
 
     return (
         <div className="TodoCounter">
-            <h1 className="TodoCounter-title">To-do list</h1>
+            <h1 className="TodoCounter-title">Modern Task Manager</h1>
             <p className="TodoCounter-date">{getCurrentDate()}</p>
         </div>
     );
